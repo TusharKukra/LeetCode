4d848037db1c5a960fba1a6dc2e4f72d2b43f621
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-11-12 10:39:59 +0530

    Update and rename TwoSum to TwoSum.cpp

--- Easy/TwoSum.cpp
+++ Easy/TwoSum.cpp
@@ -1,4 +1,4 @@
-<h3> Problem </h3>
+PROBLEM:
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -30,5 +30,5 @@ Constraints:
 Only one valid answer exists.
 
 
-<h3> Code (in C++) </h3>
+CODE:
 
